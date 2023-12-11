5dd62a53d73f51b7b6afd915d0089420d681dc6d
DerJimno <98581961+DerJimno@users.noreply.github.com>
2023-12-11 16:15:21 +0100

    add xmonad colorschemes

---
+++ .config/xmonad/lib/Colors/Monokai.hs
@@ -0,0 +1,26 @@
+module Colors.Monokai where
+
+import XMonad
+
+colorScheme = "monokai"
+
+colorBk = "#2D2A2E"
+colorFr = "#fff1f3"
+            
+color01 = "#2c2525"
+color02 = "#fd6883"
+color03 = "#adda78"
+color04 = "#f9cc6c"
+color05 = "#f38d70"
+color06 = "#a8a9eb"
+color07 = "#85dacc"
+color08 = "#fff1f3"
+            
+color09 = "#72696a"
+color10 = "#fd6883"
+color11 = "#adda78"
+color12 = "#f9cc6c"
+color13 = "#f38d70"
+color14 = "#a8a9eb"
+color15 = "#85dacc"
+color16 = "#fff1f3"
---
+++ .config/xmonad/lib/Colors/TokyoNight.hs
@@ -0,0 +1,26 @@
+module Colors.TokyoNight where
+
+import XMonad
+
+colorScheme = "tokyo-night"
+
+colorBk = "#1a1b26"
+colorFr = "#a9b1d6"
+            
+color01 = "#32344a"
+color02 = "#f7768e"
+color03 = "#9ece6a"
+color04 = "#e0af68"
+color05 = "#444b6a"
+color06 = "#ad8ee6"
+color07 = "#449dab"
+color08 = "#787c99"
+            
+color09 = "#444b6a"
+color10 = "#ff7a93"
+color11 = "#b9f27c"
+color12 = "#ff9e64"
+color13 = "#7da6ff"
+color14 = "#bb9af7"
+color15 = "#0db9d7"
+color16 = "#acb0d0"
