c2695ae3062bf84e60b52b9959326fef782a931d
jimno <98581961+DerJimno@users.noreply.github.com>
2025-02-10 12:09:05 +0100

    launch discord at chat workspace

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -170,7 +170,7 @@ myKeys conf = mkKeymap conf $
     , ("M-<Return>"        , sequence_ [spawn "alacritty", windows $ W.greedyView "dev"])                      -- launch Alacritty
     , ("M-f"               , spawn "pcmanfm")            -- launch Pcmanfm
     , ("M-b"               , spawn "brave")              -- launch Brave
-    , ("M-d"               , spawn "discord")            -- launch Discord
+    , ("M-d"               , sequence_ [spawnOn "chat" "discord", windows $ W.greedyView "chat"])              -- launch Discord
     , ("M-l"               , spawn "libreoffice")        -- launch Libreoffice
     , ("M-m"               , sequence_ [spawnOn "chat" "alacritty -e neomutt", windows $ W.greedyView "chat"]) -- launch Neomutt
 
