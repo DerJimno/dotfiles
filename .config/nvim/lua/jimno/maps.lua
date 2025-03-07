69d426c88260e8416fcf01d7d92048797d5dccfd
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-07 14:15:55 +0100

    add keybinds to comment/visual-comment

--- .config/nvim/lua/jimno/maps.lua
+++ .config/nvim/lua/jimno/maps.lua
@@ -5,7 +5,10 @@ end
 -- call vifm
 map("n", "<leader>vf", "<CMD>Vifm<CR>")
 
-
 -- Keybindings for telescope
 map("n", "<leader>fo", "<CMD>Telescope oldfiles<CR>")
 map("n", "<leader>ff", "<CMD>Telescope find_files<CR>")
+
+-- Keybindings for tpope commentary plugin
+map("n", "\\\\", "<Plug>CommentaryLine")
+map("x", "\\\\", "<Plug>Commentary")
