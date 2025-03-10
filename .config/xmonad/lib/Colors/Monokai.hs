11efc609e9ec48e5bbbd5bc08ed4db6ce492def5
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-10 14:00:42 +0100

    limit colors to only 6

--- .config/xmonad/lib/Colors/Monokai.hs
+++ .config/xmonad/lib/Colors/Monokai.hs
@@ -4,23 +4,12 @@ import XMonad
 
 colorScheme = "monokai"
 
-colorBk = "#2D2A2E"
-colorFr = "#fff1f3"
-            
-color01 = "#2c2525"
-color02 = "#fd6883"
-color03 = "#adda78"
-color04 = "#f9cc6c"
-color05 = "#a8a9eb"
-color06 = "#fa8419"
-color07 = "#85dacc"
-color08 = "#fff1f3"
-            
-color09 = "#72696a"
-color10 = "#fd6883"
-color11 = "#adda78"
-color12 = "#f9cc6c"
-color13 = "#f38d70"
-color14 = "#a8a9eb"
-color15 = "#85dacc"
-color16 = "#fff1f3"
+colorBk = "#2d2a2e"
+colorFr = "#221f22"
+ 
+color01 = "#ff6188"
+color02 = "#fc9867"
+color03 = "#ffd866"
+color04 = "#a9dc76"
+color05 = "#78dce8"
+color06 = "#ab9df2"
