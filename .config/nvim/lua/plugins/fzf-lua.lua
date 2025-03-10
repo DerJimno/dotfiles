05a405b90dc5bb0a0b4f34eb63a7825ebce08490
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-10 10:12:26 +0100

    add keybinds

--- .config/nvim/lua/plugins/fzf-lua.lua
+++ .config/nvim/lua/plugins/fzf-lua.lua
@@ -1,2 +1,16 @@
-return { 'ibhagwan/fzf-lua',
+return { 
+  "ibhagwan/fzf-lua",
+  dependencies = {"nvim-tree/nvim-web-devicons"},
+  opts = {},
+  keys = {
+    { "<leader>ff", 
+      function() require("fzf-lua").files() end,
+      desc="find files in directory"
+    }, 
+
+    { "<leader>fg", 
+      function() require("fzf-lua").live_grep() end,
+      desc="find by grepping in directory"
+    },
+  },
 }
