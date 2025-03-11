306fde8dd73aa42e475186b8ac18985cd83f24ef
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-11 14:16:54 +0100

    setup a cool function

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,4 +1,24 @@
 -- Treesitter
-return {'nvim-treesitter/nvim-treesitter',
-}
+return {
+  "nvim-treesitter/nvim-treesitter",
+  build = ":TSUpdate",
+  config = function ()
+    local configs = require("nvim-treesitter.configs")
 
+    configs.setup({
+      ensure_installed = { "lua", "vim", "ruby", "haskell", "css", "javascript", "html", "python", "json" },
+      sync_install = false,
+      highlight = { enable = true },
+      indent = { enable = true },
+      incremental_selection = {
+        enable = true,
+        keymaps = {
+          init_selection = "<Enter>", -- set to `false` to disable one of the mappings
+          node_incremental = "<Enter>",
+          scope_incremental = false,
+          node_decremental = "<Backspace>",
+        },
+      },
+    })
+  end
+}
