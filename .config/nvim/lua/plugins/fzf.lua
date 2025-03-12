20d5c4f36e00c46407fe01891c1527ba2aa77d5e
jimno <98581961+DerJimno@users.noreply.github.com>
2025-03-12 15:04:40 +0100

    add new keybinds

--- .config/nvim/lua/plugins/fzf.lua
+++ .config/nvim/lua/plugins/fzf.lua
@@ -3,14 +3,34 @@ return {
   dependencies = {"nvim-tree/nvim-web-devicons"},
   opts = {},
   keys = {
-    { "<leader>ff", 
+    { "<leader>ff",
       function() require("fzf-lua").files() end,
       desc="find files in directory"
     }, 
 
-    { "<leader>fg", 
+    { "<leader>fg",
       function() require("fzf-lua").live_grep() end,
       desc="find by grepping in directory"
+    },
+
+    { "<leader>fc",
+      function() require("fzf-lua").git_commits() end,
+      desc="find any commit"
+    },
+
+    { "<leader>fb",
+      function() require("fzf-lua").builtin() end,
+      desc="find any builtin module"
+    },
+
+    { "<leader>fh",
+      function() require("fzf-lua").helptags() end,
+      desc="find neovim help"
+    },
+
+    { "<leader>fk",
+      function() require("fzf-lua").keymaps() end,
+      desc="find your keymaps"
     },
   },
 }
