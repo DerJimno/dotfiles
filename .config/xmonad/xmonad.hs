ac92df30660bc185c35cc139dfe50d9794c8aab2
jimno <98581961+DerJimno@users.noreply.github.com>
2025-04-02 11:50:48 +0100

    reorder workspaces

--- .config/xmonad/xmonad.hs
+++ .config/xmonad/xmonad.hs
@@ -252,10 +252,10 @@ myManageHook = composeAll
     , className =? "Xmessage"           --> doMessageBox 
     , className =? "mpv"                --> doFloat 
     , className =? "Brave-browser"      --> doShift ( myWorkspaces !! 1 )
+    , className =? "Pcmanfm"            --> doShift ( myWorkspaces !! 2 )
     , className =? "libreoffice-startcenter"  
-                                        --> doShift ( myWorkspaces !! 2 )
-    , className =? "discord"            --> doShift ( myWorkspaces !! 3 )
-    , className =? "Pcmanfm"            --> doShift ( myWorkspaces !! 4 )
+                                        --> doShift ( myWorkspaces !! 3 )
+    , className =? "discord"            --> doShift ( myWorkspaces !! 4 )
     , className =? "mpv"                --> doShift ( myWorkspaces !! 5 )
     , isDialog                                --> doCenterFloat 
     ]
