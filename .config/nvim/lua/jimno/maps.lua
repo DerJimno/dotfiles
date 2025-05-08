6f1c21402a313f5e7d788579e55a31562eb682ce
jimno <98581961+DerJimno@users.noreply.github.com>
2025-05-08 11:01:44 +0100

    add commments

--- .config/nvim/lua/jimno/maps.lua
+++ .config/nvim/lua/jimno/maps.lua
@@ -1,20 +1,23 @@
-local function map(m, k, v, opts)
+local function map(m, k, v, desc, opts)
   opts = opts or {}
   opts.silent = true
+  if desc then
+    opts.desc = desc
+  end
   vim.keymap.set(m, k, v, opts)
 end
 
 -- call vifm
-map("n", "<leader>vf", "<CMD>Vifm<CR>")
+map("n", "<leader>vf", "<CMD>Vifm<CR>", "Open vifm Jim")
 
 -- Keybindings for tpope commentary plugin
-map("n", "\\\\", "<Plug>CommentaryLine")
-map("x", "\\\\", "<Plug>Commentary")
+map("n", "\\\\", "<Plug>CommentaryLine", "Comment/uncomment Line")
+map("x", "\\\\", "<Plug>Commentary", "Comment/uncomment selected Line/s")
 
 -- enable <CR> to work wirh coc (autocompletion plugin)
-map("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : \"\\<CR>\"", { expr = true, noremap = true })
+map("i", "<CR>", "coc#pum#visible() ? coc#pum#confirm() : \"\\<CR>\"", "" , { expr = true, noremap = true })
 
 -- call oil (buffer file manager)
-map("n", "-", "<CMD>Oil --float --preview<CR>")
+map("n", "-", "<CMD>Oil --float --preview<CR>", "Open Oil File Manager" )
 
-map("n", "gl", function() vim.diagnostic.open_float() end) 
+map("n", "gl", function() vim.diagnostic.open_float() end, "Show Diagnostics")
