68e5103d7f97419099bcfcb5a3e20128d7e175ea
jimno <98581961+DerJimno@users.noreply.github.com>
2025-05-17 19:43:00 +0100

    use less to show keybinds from start not end

--- .config/xmonad/lib/xmonad-keybinds.sh
+++ .config/xmonad/lib/xmonad-keybinds.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-sed -n '/KEYBINDINGS/,/Switchers/p' .config/xmonad/xmonad.hs | grep -e ', ("' -e '\[ ("' -e 'KB_group' | sed -e 's/^[ \t]*//' -e 's/, (/(/' -e 's/\[ //' -e 's/-- KB_group /\n\t/' -e 's/, .*-- /\t/' -e 's/\("[^"]*"\)\([^"]*\)/\1\)\2/' -e 's/"//g' 
+sed -n '/KEYBINDINGS/,/Switchers/p' .config/xmonad/xmonad.hs | grep -e ', ("' -e '\[ ("' -e 'KB_group' | sed -e 's/^[ \t]*//' -e 's/, (/(/' -e 's/\[ //' -e 's/-- KB_group /\n\t/' -e 's/, .*-- /\t/' -e 's/\("[^"]*"\)\([^"]*\)/\1\)\2/' -e 's/"//g' | less
